f4eb5b857f82aaf0b84d5ebb1091e7107d458e87
Brice JUHEL <juhel.brice@gmail.com>
2023-10-27 12:07:12 +0200

    ajout fixtures products et images

--- src/DataFixtures/CategoriesFixtures.php
+++ src/DataFixtures/CategoriesFixtures.php
@@ -12,6 +12,8 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 class CategoriesFixtures extends Fixture
 {
 
+    private $counter = 1;
+
     public function __construct(private SluggerInterface $slugger){}
 
     public function load(ObjectManager $manager): void
@@ -38,6 +40,9 @@ class CategoriesFixtures extends Fixture
         $category->setSlug($this->slugger->slug($category->getName())->lower());
         $category->setParent($parent);
         $manager->persist($category);
+
+        $this->addReference('cat-'.$this->counter, $category);
+        $this->counter++;
 
         return $category;
     }
--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -2,16 +2,57 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Users;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Faker;
+
 ;
 
 class UsersFixtures extends Fixture
 {
+
+    public function __construct(private UserPasswordHasherInterface $passwordEncoder, private SluggerInterface $slugger)
+    {        
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $admin = new Users();
+        $admin->setEmail('admin@test.com');
+        $admin->setLastname('Doh');
+        $admin->setFirstname('John');
+        $admin->setAddress('1 rue du Test');
+        $admin->setZipCode('95000');
+        $admin->setCity('Paris');
+        $admin->setPassword(
+            $this->passwordEncoder->hashPassword($admin, 'admin')
+        );
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        $faker = Faker\Factory::create('fr_FR');
+
+        for($usr = 1; $usr <=5; $usr++){
+
+            $user = new Users();
+            $user->setEmail($faker->email);
+            $user->setLastname($faker->lastName);
+            $user->setFirstname($faker->firstName);
+            $user->setAddress($faker->streetAddress);
+            $user->setZipCode(str_replace(' ', '', $faker->postcode));
+            $user->setCity($faker->city);
+            $user->setPassword(
+                $this->passwordEncoder->hashPassword($user, 'secret')
+            );
+            $manager->persist($user);
+
+        }
+
+
 
         $manager->flush();
     }
